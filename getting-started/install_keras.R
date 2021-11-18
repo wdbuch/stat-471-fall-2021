2bc93326bc366076ffc4db648e989f5a9ab1596a
ekatsevi <katsevich.gene@gmail.com>
2021-11-18 00:00:09 -0500

    Added installation scripts for deep learning dependencies

--- getting-started/install_keras.R
+++ getting-started/install_keras.R
@@ -1,13 +1,15 @@
 # install helpers (modified from https://rdrr.io/cran/ISLR2/src/inst/helpers/install.R)
-source("install_helpers.R")
+source("install_keras_helpers.R")
 
 # remove existing installations
+cat(sprintf("Removing existing installations of keras, tensorflow, reticulate (if any)...\n"))
 tryCatch(
   remove.packages(c("keras", "tensorflow", "reticulate")),
   error = function(e) "Some or all packages not previously installed, that's ok!"
 )
 
 # install keras
+cat(sprintf("Installing keras...\n"))
 install.packages("keras", repos = 'https://cloud.r-project.org')
 
 # update environment variables
@@ -21,10 +23,15 @@ Sys.setenv(RETICULATE_AUTOCONFIGURE='FALSE',
                                                    mustWork = FALSE))
 
 # install separate miniconda, called islr-miniconda
+cat(sprintf("Installing miniconda...\n"))
 install_miniconda()
 
 # install Tensorflow
+cat(sprintf("Installing Tensorflow...\n"))
 install_tensorflow()
 
 # check Python configuration
 print_py_config()
+
+# done
+cat(sprintf("Installation complete!\n"))
